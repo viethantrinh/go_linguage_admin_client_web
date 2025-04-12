79e9f82d55c5fab81daeb43d58dac28317ee2c30
viethantrinh <hntrnn12@gmail.com>
2025-04-13 05:43:56 +0700

    matching exercise

---
+++ src/app/features/exercise/matching-exercise/matching-exercise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gl-matching-exercise',
+  imports: [],
+  templateUrl: './matching-exercise.component.html',
+  styleUrl: './matching-exercise.component.scss'
+})
+export class MatchingExerciseComponent {
+
+}
